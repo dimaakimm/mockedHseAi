99349aa2ea1eaa93eb4f72d0755c62933aa131d9
dimaakimm <dmitriykim2005@gmail.com>
2025-09-02 20:51:03 +0300

    fix(popupChat): изменена максимальная ширина chat'а

---
+++ src/environments/environment.ts
@@ -0,0 +1,12 @@
+export const environment = {
+  production: false,
+  apiBaseUrl: 'https://platform.stratpro.hse.ru/pu-vleviczkaya-pa-classifier',
+  authToken: 'Basic ZGV2ZWxvcGVyOmExNTU2MmViYjNjOTk3NmU=',
+  classifierEndpoint: '/classifier/predict',
+  aiEndpoint: '/ml/generate',
+  request: {
+    maxRetries: 2,
+    timeoutMs: 15000,
+    baseRetryDelayMs: 300,
+  },
+};
